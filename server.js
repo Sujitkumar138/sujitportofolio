5b75f8099ada664c19bb71807b553375e5ca5a78
Sujitkumar138 <mishrakumarsujit8@gmail.com>
2024-11-12 19:11:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-// Import necessary modules
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path= require('path')
+const path = require("path");
+
 // Load environment variables from .env file
 dotenv.config();
 
 // Verify that the environment variable is loaded correctly
 console.log("Web3 API Key:", process.env.WEB3_FORM_API_KEY); // Should print your API key
-
-// Rest of your code...
 
 // Initialize express app
 const app = express();
@@ -17,15 +15,17 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-//static file access
-app.use(express.static(path.join(--dirname,"./client/build")))
+
+// Static file access
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // Import routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
-app.get('*', function(req, res) {
-  res.sendFile(path. join(--dirname,'/client/build/index.html'));
-});
 
+// Catch-all handler for any request not matched by other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 
 // Define port (use PORT from .env or default to 8080)
 const PORT = process.env.PORT || 8080;
